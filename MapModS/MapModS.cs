159055e98ba937cb136853a5ef3c82b641c2ef0e
Sebastian Young Ye <yye3028@gmail.com>
2022-01-28 02:26:09 +1100

    Update version number

--- MapModS/MapModS.cs
+++ MapModS/MapModS.cs
@@ -16,7 +16,7 @@ namespace MapModS
     {
         public static MapModS Instance;
 
-        private readonly string _version = "2.1.0 DEBUG";
+        private readonly string _version = "2.1.0 PRE-RELEASE";
 
         public override string GetVersion() => _version;
 
