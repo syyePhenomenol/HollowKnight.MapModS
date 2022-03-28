90a3e5a9e7a2cfc412574e247041b444418c8ce3
syyePhenomenol <yye3028@gmail.com>
2022-03-28 19:43:47 +1100

    Bump version

--- MapModS/MapModS.cs
+++ MapModS/MapModS.cs
@@ -16,7 +16,7 @@ namespace MapModS
     {
         public static MapModS Instance;
 
-        public override string GetVersion() => "2.3.2";
+        public override string GetVersion() => "2.3.3";
 
         public override int LoadPriority() => 10;
 
